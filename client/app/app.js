b72d025ac845de9d29cd590dd5a07a7298073d9a
karan.bisht <karan.bisht>
2015-12-19 14:43:07 +0530

    Initial Commint

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('nodeYoamanApp', [
+  'nodeYoamanApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
